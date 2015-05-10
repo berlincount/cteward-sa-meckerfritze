b828dca094cd3cea040a7af97346536274a0b5bf
Andreas 'count' Kotes <count@flatline.de>
2015-05-10 23:39:58 +0200

    checks for external email address

--- checks/illegal_external_mail.py
+++ checks/illegal_external_mail.py
@@ -1,2 +1,28 @@
-#$#
-# ** Externe Mailadresse hat ungueltiges Format
+import re
+
+def check_member(member_raw):
+    if not member_raw['Telefon3'] or not re.compile('^[^@]+@[^@]+\.[^@]+$').match(member_raw['Telefon3']):
+        return (False, "invalid external mailaddress '%s'" % member_raw['Telefon3'])
+    return (True,)
+
+import unittest
+class Testcases(unittest.TestCase):
+    def test_success(self):
+        self.assertEqual(check_member({
+            'Telefon3': 'user@example.com',
+        }),(True,))
+
+    def test_fail_mail_empty(self):
+        self.assertEqual(check_member({
+            'Telefon3': '',
+        }),(False, "invalid external mailaddress ''"))
+
+    def test_fail_mail_bogus(self):
+        self.assertEqual(check_member({
+            'Telefon3': 'blah blah blah',
+        }),(False, "invalid external mailaddress 'blah blah blah'"))
+
+    def test_fail_mail_multiple(self):
+        self.assertEqual(check_member({
+            'Telefon3': 'user@example.com,user@example.com',
+        }),(False, "invalid external mailaddress 'user@example.com,user@example.com'"))
--- checks/internal_external_mail.py
+++ checks/internal_external_mail.py
@@ -1,2 +1,16 @@
-#$#
-# ** Externe Mailadresse endet @c-base.org
+def check_member(member_raw):
+    if member_raw['Telefon3'] and member_raw['Telefon3'].endswith('@c-base.org'):
+        return (False, "external mailaddress points to internal mailaddress '%s'" % member_raw['Telefon3'])
+    return (True,)
+
+import unittest
+class Testcases(unittest.TestCase):
+    def test_success(self):
+        self.assertEqual(check_member({
+            'Telefon3': 'user@example.com',
+        }),(True,))
+
+    def test_fail(self):
+        self.assertEqual(check_member({
+            'Telefon3': 'user@c-base.org'
+        }),(False, "external mailaddress points to internal mailaddress 'user@c-base.org'"))

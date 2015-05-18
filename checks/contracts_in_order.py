cad33e9e2cd347baf66bb1d0517cbd3aa7c60234
Andreas 'count' Kotes <count@flatline.de>
2015-05-18 23:39:29 +0200

    contract overlap / gap / order detection improvements

---
+++ checks/contracts_in_order.py
@@ -0,0 +1,85 @@
+import dateutil.parser
+from dateutil.tz import tzutc
+import datetime
+import itertools
+
+from pprint import pprint
+
+def check_member(member_raw, contracts_raw):
+    if not member_raw['Eintritt'] or len(contracts_raw) <= 1:
+        return (True,)
+
+    message = ''
+    lastend = '1970-01-01T00:00:00.000Z'
+    lastnum = 0
+    for contract in contracts_raw:
+        if contract['VertragBegin'] < lastend:
+            if message != '':
+                message += ', '
+            message += "contracts %d and %d seem out of order" % (lastnum,int(contract['VertragNr']))
+        lastend = contract['VertragEnde']
+        lastnum = int(contract['VertragNr'])
+
+    if message != '':
+        return (False, message)
+    else:
+        return (True,)
+
+import unittest
+class Testcases(unittest.TestCase):
+    def test_success_no_contracts(self):
+        self.assertEqual(check_member({
+            'Eintritt': 'set-to-something',
+        },[{}]),(True,))
+
+    def test_success_single_contract(self):
+        self.assertEqual(check_member({
+            'Eintritt': 'set-to-something',
+        },[{
+            'VertragBegin': '2015-01-01T00:00:00.000Z'
+        }]),(True,))
+
+    def test_success_multiple_contracts(self):
+        self.assertEqual(check_member({
+            'Eintritt': 'set-to-something',
+        },[{
+            'VertragBegin': '2015-04-01T00:00:00.000Z',
+            'VertragEnde':  '2015-04-30T00:00:00.000Z'
+            },{
+            'VertragBegin': '2015-05-01T00:00:00.000Z',
+            'VertragEnde':  '2015-05-31T00:00:00.000Z'
+        }]),(True,))
+
+    def test_success_multiple_contracts(self):
+        self.assertEqual(check_member({
+            'Eintritt': 'set-to-something',
+        },[{
+            'VertragNr':    1,
+            'VertragBegin': '2015-01-01T00:00:00.000Z',
+            'VertragEnde':  '2015-01-31T00:00:00.000Z'
+            },{
+            'VertragNr':    2,
+            'VertragBegin': '2015-02-01T00:00:00.000Z',
+            'VertragEnde':  '2015-02-28T00:00:00.000Z'
+            },{
+            'VertragNr':    3,
+            'VertragBegin': '2015-03-01T00:00:00.000Z',
+            'VertragEnde':  '2015-03-31T00:00:00.000Z'
+        }]),(True,))
+
+    def test_fail_multiple_contracts(self):
+        self.assertEqual(check_member({
+            'Eintritt': 'set-to-something',
+        },[{
+            'VertragNr':    1,
+            'VertragBegin': '2015-01-01T00:00:00.000Z',
+            'VertragEnde':  '2015-01-31T00:00:00.000Z'
+            },{
+            'VertragNr':    2,
+            'VertragBegin': '2015-03-01T00:00:00.000Z',
+            'VertragEnde':  '2015-03-31T00:00:00.000Z'
+            },{
+            'VertragNr':    3,
+            'VertragBegin': '2015-02-01T00:00:00.000Z',
+            'VertragEnde':  '2015-02-28T00:00:00.000Z'
+        }]),(False,'contracts 2 and 3 seem out of order'))
